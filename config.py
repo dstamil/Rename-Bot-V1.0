fa5639f9cd986b6cae42fbbacbe34829983336eb
dstamil <99588972+dstamil@users.noreply.github.com>
2023-02-11 11:08:34 +0530

    Update config.py
    Signed-off-by: dstamil <99588972+dstamil@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "14318701")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "078057f70624f9143c45b5506c8dd617")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5777835679:AAH5Ga4t9VhYXSB7tHr_dDdON9TGaNHiCn8") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "Movie_Zone_C") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","Gowthaman")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Gowthaman:@cluster0.ounmfkw.mongodb.net/?retryWrites=true&w=majority")
  
-FLOOD = int(os.environ.get("FLOOD", "10"))
+FLOOD = int(os.environ.get("FLOOD", "0"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
 
